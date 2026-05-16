5a73fb9ad9b7413f3bb3ee5c6a7aec3fe735b4b3
SpartaQ <theshiek@gmail.com>
2026-05-16 10:44:41 +0200

    dist

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.lf175s07c88"
+    "revision": "0.o36rprgbpng"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
